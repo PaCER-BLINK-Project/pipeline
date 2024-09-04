d26beec15ac3b3f5f755817c231f4128f595e2ee
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-09-04 15:24:30 +0800

    added scripts to average images

---
+++ experiments/avg_n_images.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --account=director2183-gpu # your account pawsey0809-gpu or director2183-gpu
+#SBATCH --partition=gpu            # Using the gpu partition
+#SBATCH --time=23:59:59
+#SBATCH --ntasks-per-node=1        # Set this for 1 mpi task per compute device
+#SBATCH --gres=gpu:1
+#SBATCH --gpu-bind=closest         # Bind each MPI taks to the nearest GPU
+#SBATCH --output=./avg_n_images.o%j
+#SBATCH --error=./avg_n_images.e%j
+#SBATCH --export=NONE
+
+module reset 
+module unload gcc/12.2.0 
+module swap pawseyenv/2024.05 pawseyenv/2023.08 
+module load gcc/12.2.0
+module use /software/projects/director2183/msok/setonix/2023.08/modules/zen3/gcc/12.2.0/ /software/projects/director2183/setonix/2023.08/modules/zen3/gcc/12.2.0 /software/setonix/2023.08/modules/zen3/gcc/12.2.0/libraries
+module load msfitslib/devel 
+
+n_avg=128
+if [[ -n "$1" && "$1" != "-" ]]; then
+   n_avg=$1
+fi
+
+list=fits_list
+if [[ -n "$2" && "$2" != "-" ]]; then
+   list=$2
+fi
+
+fits_count=`cat $list|wc -l`
+start_index=0
+while [[ $start_index -lt $fits_count ]];
+do
+   i_str=`echo $start_index | awk '{printf("%05d\n",$1);}'`
+   awk -v n_avg=$n_avg -v start=${start_index} '{if(NR>start && NR<(start+n_avg+1)){print $0;}}' $list > avg_list_${i_str}
+   
+   echo "avg_images avg_list_${i_str} avg_${i_str}.fits rms_${i_str}.fits -r 10000000.00"
+   avg_images avg_list_${i_str} avg_${i_str}.fits rms_${i_str}.fits -r 10000000.00
+   
+   start_index=$(($start_index+$n_avg))   
+done
---
+++ experiments/avg_per_timestamp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+n_avg=128
+if [[ -n "$1" && "$1" != "-" ]]; then
+   n_avg=$1
+fi
+
+n_times=10
+if [[ -n "$2" && "$2" != "-" ]]; then
+   n_times=$2
+fi
+
+t=0
+while [[ $t -lt $n_times ]];
+do
+   t_str=`echo $t | awk '{printf("%06d\n",$1);}'`
+   ls ch???/test_image_time${t_str}_ch?????_real.fits > fits_list_${t_str}
+   
+   echo "sbatch ./avg_n_images.sh ${n_avg} fits_list_${t_str}"
+   sbatch ./avg_n_images.sh ${n_avg} fits_list_${t_str}
+   
+   t=$(($t+1))
+done
